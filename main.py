eb3fa4be01c98e6653801497cfe21ba8ef80ee86
Cristian Palos Rejano <cpalosrejano@gmail.com>
2025-03-08 21:53:07 +0100

    [wip] collect studio ghibli information

--- main.py
+++ main.py
@@ -152,6 +152,90 @@ def get_all_movies(lang: str) -> List[Movie]:
             rt_score="98"
         ),
         Movie(
+            id="b79c22e7-a35e-44f8-a01d-786928ac9761",
+            title="Only Yesterday",
+            description="Only Yesterday revolves around Taeko, a single woman working a desk job in Tokyo in 1982, taking a vacation in the countryside with the family of her sister in-law. During her vacation, Taeko finds herself looking back at her time as a young schoolgirl growing up in 1966. The film flips back and forth between the two time periods with a lot of nostalgia and beautiful country scenery as Taeko sorts out her flashbacks and tries to make some tough decisions about her future.",
+            title_romanised="Omohide Poro Poro",
+            image_cartel="https://resizing.flixster.com/AhWAif9MU5TIoe8QD0KsxRi_4go=/fit-in/705x460/v2/https://resizing.flixster.com/GMXOIc2qji2sLUuSxsvngLTkb3M=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzL2JlNWVkYTQ2LWVjN2EtNDE0ZS1hZjkzLTcxNzFmNzJhMTc3YS53ZWJw",
+            image_banner="https://resizing.flixster.com/-yAst9h9BfApu7UkKEfr01650bg=/fit-in/705x460/v2/https://resizing.flixster.com/3VGRBUenNni8029YCcGYsWyvStM=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzBmZDJjOWI3LTBiYzItNDVjYy04MDEyLTAzNGJhNzJiMzc4MS53ZWJw",
+            director="Isao Takahata",
+            producer="Hayao Miyazaki, Toshio Susuki, Yasuyoshi Tokuma, Yoshio Sasaki, Ritsuo Isobe",
+            soundtrack="Masaru Hoshi",
+            release_date="1991",
+            running_time="118",
+            rt_score="100"
+        ),
+        Movie(
+            id="ea4c45df-bd15-4ef0-83f9-f6f9dca675cb",
+            title="Porco Rosso",
+            description="In Early 1930's era Italy, air pirates, bounty hunters and high flyers of all sorts rule the skies. The most cunning and skilled of these pilots is Porco Rosso, a man cursed with the head of a pig after watching the spirits of the pilots killed in the last air battle he fought in rise to the heavens. He now makes a living taking jobs, such as rescuing those kidnapped by air pirates. Donald Curtis, Porco's rival in the air and in catching the affections of women, provides a constant challenge to the hero, culminating in a hilarious, action packed finale.",
+            title_romanised="Kurenai no buta",
+            image_cartel="https://resizing.flixster.com/B_5NwogQmCjMh-3GSZIHf-19ySk=/fit-in/705x460/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p160147_p_v8_ac.jpg",
+            image_banner="https://resizing.flixster.com/nMfGpB37NFsCG97Jr0zmFXtoGyE=/fit-in/705x460/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/82008_ba.jpg",
+            director="Hayao Miyazaki",
+            producer="Toshio Suzuki",
+            soundtrack="Joe Hisaishi",
+            release_date="1992",
+            running_time="94",
+            rt_score="96"
+        ),
+        Movie(
+            id="1fe5ba48-40ad-4059-ba8f-37ee9ff4d8c4",
+            title="Ocean Waves",
+            description="The main character travels to his hometown for his high school reunion. During the trip, he recalls the memories of the days in high school. Friendship, subtle love, a trip to Tokyo and so on all came back to him as the film evolves.",
+            title_romanised="Umi ga kikoeru",
+            image_cartel="https://resizing.flixster.com/2TL-KGDgtjPkMiESKwCq_Yrdvkg=/fit-in/705x460/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p8477698_v_v8_ad.jpg",
+            image_banner="https://resizing.flixster.com/tQzIAhqVovYBRdckGrtCiGX804g=/fit-in/705x460/v2/https://resizing.flixster.com/8P2iU5EuSGQX1J-S77ukFyo5pw8=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzE3NmZmMDhmLWU2M2QtNDVlYi1hMmU1LWIxYTlhYWZlZDQ4NC53ZWJw",
+            director="Tomomi Mochizuki",
+            producer="Nozomu Takahashi, Toshio Suzuki, Seiji Okuda",
+            soundtrack="Shigeru Nagata",
+            release_date="1993",
+            running_time="72",
+            rt_score="89"
+        ),
+        Movie(
+            id="caca9a5a-6e05-47df-9ef0-de57508ecea9",
+            title="Pom Poko",
+            description="As the human city development encroaches on the raccoon population's forest and meadow habitat, the raccoons find themselves faced with the very real possibility of extinction. In response, the raccoons engage in a desperate struggle to stop the construction and preserve their home.",
+            title_romanised="Heisei Tanuki Gassen Ponpoko",
+            image_cartel="https://resizing.flixster.com/TXvyfbacStvaXZDIYkOscIbWJBg=/fit-in/705x460/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p24121_p_v8_ad.jpg",
+            image_banner="https://resizing.flixster.com/-dxE5pVqANoWz3jqbuSOP2Wq7dc=/fit-in/705x460/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p24121_i_h10_ac.jpg",
+            director="Isao Takahata",
+            producer="Toshio Suzuki, Hayao Miyazaki",
+            soundtrack="Chang Chang Typhoon",
+            release_date="1994",
+            running_time="119",
+            rt_score="86"
+        ),
+        Movie(
+            id="07b21060-7236-4939-a414-d9c3b868eefe",
+            title="Whisper of the Heart",
+            description="A young girl finds that all the books she chooses in the library have been previously checked out by the same boy. Later she meets a very infuriating fellow... could it be her \"friend\" from the library? The boy's grandfather has a violin sales and service shop. The boy wants to be a violin maker like his grandfather.",
+            title_romanised="Mimi wo sumaseba",
+            image_cartel="https://resizing.flixster.com/yXm0ME9MxTd04enCKbhkcUISs4g=/fit-in/705x460/v2/https://resizing.flixster.com/iOYqoVyKuD1A_JnUrUlTE6BkCeM=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzL2VjMGM0MDRjLWZlMDItNGU5OS05MzIzLTMxZjczYTNmYmI3Yi5qcGc=",
+            image_banner="https://resizing.flixster.com/97pmKEMrKYHXAEnNk5SDu9B_cEI=/fit-in/705x460/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p160148_i_h11_ab.jpg",
+            director="Yoshifumi Kondô",
+            producer="Toshio Suzuki, Yasuyoshi Tokuma",
+            soundtrack="Yuji Nomi",
+            release_date="1995",
+            running_time="111",
+            rt_score="95"
+        ),
+        Movie(
+            id="329b6ac9-b6cc-426d-bf20-4deff5be1f5f",
+            title="Princess Mononoke",
+            description="A prince is infected with an incurable disease by a possessed boar/god. He is to die unless he can find a cure to rid the curse from his body. It seems that his only hope is to travel to the far east. When he arrives to get help from the deer god, he finds himself in the middle of a battle between the animal inhabitants of the forest and an iron mining town that is exploiting and killing the forest. Leading the forest animals in the battle is a human raised by wolves, Princess Mononoke.",
+            title_romanised="Mononoke-hime",
+            image_cartel="https://resizing.flixster.com/mv1ttpDFqM1EC0iIRO-PoEQUX5s=/fit-in/705x460/v2/https://resizing.flixster.com/fLUbEbfgnDvNc1kHMC62jFcMny0=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzL2QxM2YyODA1LThhYWItNGU4Zi1iMjk3LTg4YmZhMGE3ZTQwMy5qcGc=",
+            image_banner="https://resizing.flixster.com/Uh3LSoauJYH1R1qGK3i8TGRU7zw=/fit-in/705x460/v2/https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p23052_i_h10_aa.jpg",
+            director="Hayao Miyazaki",
+            producer="Toshio Suzuki",
+            soundtrack="Joe Hisaishi",
+            release_date="1997",
+            running_time="133",
+            rt_score="93"
+        ),
+        Movie(
             id="",
             title="",
             description="",
@@ -172,12 +256,12 @@ def get_all_movies(lang: str) -> List[Movie]:
 ### 3. La tumba de luciérnagas (https://www.filmaffinity.com/es/film582716.html)
 ### 4. Mi vecino Totoro (https://www.filmaffinity.com/es/film646631.html)
 ### 5. Kiki: Entregas a domicilio (https://www.filmaffinity.com/es/film583615.html)
-# 6. Recuerdos del ayer (https://www.filmaffinity.com/es/film235776.html)
-# 7. Porco Rosso (https://www.filmaffinity.com/es/film169128.html)
-# 8. Puedo escuchar el mar (https://www.filmaffinity.com/es/film747473.html)
-# 9. Pompoko (https://www.filmaffinity.com/es/film622966.html)
-# 10. Susurros del corazón (https://www.filmaffinity.com/es/film920920.html)
-# 11. La princesa Mononoke (https://www.filmaffinity.com/es/film890814.html)
+### 6. Recuerdos del ayer (https://www.filmaffinity.com/es/film235776.html)
+### 7. Porco Rosso (https://www.filmaffinity.com/es/film169128.html)
+### 8. Puedo escuchar el mar (https://www.filmaffinity.com/es/film747473.html)
+### 9. Pompoko (https://www.filmaffinity.com/es/film622966.html)
+### 10. Susurros del corazón (https://www.filmaffinity.com/es/film920920.html)
+### 11. La princesa Mononoke (https://www.filmaffinity.com/es/film890814.html)
 # 12. Mis vecinos los Yamada (https://www.filmaffinity.com/es/film348798.html)
 # 13. El viaje de Chihiro (https://www.filmaffinity.com/es/film759533.html)
 # 14. Haru en el reino de los gatos (https://www.filmaffinity.com/es/film520479.html)
