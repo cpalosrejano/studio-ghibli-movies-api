8c5813df5a5b8a606dca9653274bb6181b4357de
Cristian Palos Rejano <cpalosrejano@gmail.com>
2025-03-10 22:59:06 +0100

    created spanish translation for movies

--- app/translations/movie/es.py
+++ app/translations/movie/es.py
@@ -1,12 +1,104 @@
 from app.translations.schema import MovieTranslation
 
 translations = {
-    "My Neighbor Totoro": MovieTranslation(
-        title = "Mi vecino Totoro",
-        description = "Una historia sobre dos niñas y sus aventuras con Totoro."
-    ),
-    "Spirited Away": MovieTranslation (
-        title = "El viaje de Chihiro",
-        description = "Una niña entra en un mundo misterioso y debe encontrar el camino de regreso a casa."
-    )
-}
+     "Nausicaä of the Valley of the Wind" : MovieTranslation(
+          title = "Nausicaä del Valle del Viento",
+          description = "En un futuro lejano, mil años después de una guerra de carácter apocalíptico, la Tierra aparece cubierta de bosques plagados de hongos venenosos y de insectos gigantescos. Los hombres han sido diezmados; los supervivientes viven en algunos poblados aislados y sobreviven a duras penas en las cercanías de un bosque contaminado con gases tóxicos e insectos mutantes gigantes, que cubren gran parte de la Tierra. Nausicaä es la princesa de El Valle de Viento, un reino minúsculo, rodeado de reinos más poderosos y hostiles. Es una guerrera que sabe pilotar naves, pero es también compasiva, tanto que se resiste a ver a los insectos como enemigos, sobre todo a los Ohms, artrópodos gigantescos y temibles por los que siente una extraña simpatía. La crisis estalla cuando el reino vecino de Tolmekia, encabezado por la princesa Kushana, invade El Valle del Viento."
+     ),
+     "Castle in the Sky" : MovieTranslation(
+          title = "El castillo en el cielo",
+          description = "Una aeronave se desliza sobre un mar de nubes, en una noche de luna llena. Muska, un agente secreto del gobierno, acompaña a una chica llamada Sheeta a la fortaleza de Tedis. Repentinamente la nave es atacada por los piratas que, al igual que el gobierno, buscan el secreto de la piedra mágica de levitación que Sheeta lleva alrededor del cuello. La piedra es la llave que abrirá las puertas de La Fortaleza celeste, una isla flotante en medio del cielo creada por una misteriosa raza que hace mucho tiempo desapareció del planeta. Pazu, un joven muchacho, se hace amigo de Sheeta, le ayuda a escapar de sus seguidores y juntos se disponen a resolver el misterio de la Fortaleza Celeste. Cuando Sheeta y Pazu inician su viaje hacia la Fortaleza Celeste, ponen en marcha una cadena de acontecimientos irreversibles. En este misteriosos lugar encontrarán un tesoro mucho más grande que el poder de gobernar el mundo."
+     ),
+     "Grave of Fireflies" : MovieTranslation(
+          title = "La tumba de las luciérnagas",
+          description = "Segunda Guerra Mundial (1939-1945). Seita y Setsuko son hijos de un oficial de la marina japonesa que viven en Kobe. Un día, durante un bombardeo, no consiguen llegar a tiempo al búnker donde su madre los espera. Cuando después buscan a su madre, la encuentran malherida en la escuela, que ha sido convertida en un hospital de urgencia"
+     ),
+     "My Neighbor Totoro" : MovieTranslation(
+          title = "Mi vecino Totoro",
+          description = "En los años 50, una familia japonesa se traslada al campo. Las dos hijas, Satsuki y Mei, entablan amistad con Totoro, un espíritu del bosque. El padre es un profesor universitario que estimula la imaginación de sus hijas relatándoles fábulas e historias mágicas sobre duendes, fantasmas y espíritus protectores de los hogares, mientras la madre se encuentra enferma en el hospital."
+     ),
+     "Kiki's Delivery Service" : MovieTranslation(
+          title = "Nicky, la aprendiz de bruja",
+          description = "Kiki (o Nicky en el doblaje español) es una joven bruja de 13 años, en periodo de entrenamiento, que se divierte volando en su escoba junto a Jiji, un sabio gato negro. Según la tradición, todas las brujas de esa edad deben abandonar su hogar durante un año para saber valerse por sí mismas. Así, ella descubrirá lo que significa la responsabilidad, la independencia y la amistad. En su camino Kiki (Nicky) y Jiji harán un nuevo amigo, Tombo, con el que vivirán extraordinarias aventuras."
+     ),
+     "Only Yesterday" : MovieTranslation(
+          title = "Recuerdos del ayer",
+          description = "Taeko Okajima es una joven que pide permiso de vacaciones para poder regresar a su casa de Yamagata, el pueblo de su cuñada, y donde vivió los momentos más felices de su vida. En el transcurso de su viaje a la granja de sus familiares y a su posterior estancia, Taeko irá recordando su pasado, mientras vive intensamente su presente, sin saber muy bien qué hacer con su vida."
+     ),
+     "Porco Rosso" : MovieTranslation(
+          title = "Porco Rosso",
+          description = "Periodo de entreguerras. Porco es un cerdo aviador que frustra todos los actos de piratería perpetrados por los piratas aéreos del Adriático. Éstos, decididos a acabar con el valiente y hábil aviador, se ponen de acuerdo para contratar a un aventurero americano cuya misión será eliminarlo."
+     ),
+     "Ocean Waves" : MovieTranslation(
+          title = "Puedo escuchar el mar",
+          description = "Una chica de Tokio acaba de ser trasladada a una de las escuelas superiores de la ciudad de Kochi. Es guapa, buena en los deportes y en los estudios, pero de algún modo no consigue adaptarse a la vida social de la escuela. A ese mismo colegio pertenecen Taku Morisaki y Yukata Matsuno, dos grandes amigos, el primero de los cuales empieza a interesarse visiblemente por la recién llegada Muto."
+     ),
+     "Pom Poko" : MovieTranslation(
+          title = "Pompoko",
+          description = "Ante la inminente destrucción de un frondoso bosque cercano a una ciudad, cuyos dirigentes quieren construir en su lugar una urbanización, los mapaches que viven en el bosque intentarán sabotear la operación para así conservar sus hogares."
+     ),
+     "Whisper of the Heart" : MovieTranslation(
+          title = "Susurros del corazón",
+          description = "Una joven estudiante amante de los libros descubre que todos los libros que ha elegido en la biblioteca han sido previamente elegidos por una misma persona. Cuando descubre quién es conoce a Seiji, un joven que está aprendiendo el arte de fabricar violines."
+     ),
+     "Princess Mononoke" : MovieTranslation(
+          title = "La princesa Mononoke",
+          description = "Con el fin de curar la herida que le ha causado un jabalí enloquecido, el joven Ashitaka sale en busca del dios Ciervo, pues sólo él puede liberarlo del sortilegio. A lo largo de su periplo descubre cómo los animales del bosque luchan contra hombres que están dispuestos a destruir la Naturaleza."
+     ),
+     "My Neighbors the Yamadas" : MovieTranslation(
+          title = "Mis vecinos los Yamadas",
+          description = "Basada en el famoso manga de Hisachi Ishii, esta película cuenta pequeñas e imaginativas historias de la vida diaria de una típica familia de clase media de Tokyo. Los Yamada son Takashi y Matsuko (el padre y la madre), Shige (la suegra), Noboru (el hijo), Nonoko (la hija) y Pochi (el perro de la familia)."
+     ),
+     "Spirited Away" : MovieTranslation(
+          title = "El viaje de Chihiro",
+          description = "Chihiro es una niña de diez años que viaja en coche con sus padres. Después de atravesar un túnel, llegan a un mundo fantástico, en el que no hay lugar para los seres humanos, sólo para los dioses de primera y segunda clase. Cuando descubre que sus padres han sido convertidos en cerdos, Chihiro se siente muy sola y asustada."
+     ),
+     "The Cat Returns" : MovieTranslation(
+          title = "Haru en el reino de los gatos",
+          description = "Esta es la historia de Haru, una joven cuya suerte cambiará cuando un día, al volver del instituto, salva a un gato de ser atropellado por un camión. Pero no es un gato común, es Lune, el Príncipe del Reino de los Gatos. En recompensa por su acción, Haru es invitada a pasar un tiempo en el reino de Lune."
+     ),
+     "Howl's Moving Castle" : MovieTranslation(
+          title = "El castillo ambulante",
+          description = "Narra la historia de Sophie, una joven sobre la que pesa una horrible maldición que le confiere el aspecto de una anciana. Sophie decide pedir ayuda al mago Howl, que vive en un castillo ambulante, pero tal vez sea Howl quien necesite la ayuda de Sophie."
+     ),
+     "Tales from Earthsea" : MovieTranslation(
+          title = "Cuentos de Terramar",
+          description = "Película de anime del Studio Ghibli dirigida por Goro Miyazaki, hijo de Hayao Miyazaki. Está basada en el tercer libro de la serie \"Terramar: La costa más lejana\", escrita por Ursula K. Le Guin. Una adaptación en manga se ha publicado en Japón. La historia nos sitúa en el archipiélago de Terramar, donde hay dragones, magos y espectros, talismanes y poderes. Es un mundo gobernado por la magia y, ante todo, por las palabras, pues cada cosa posee su nombre verdadero, el designado durante la Creación, que otorga a los hechiceros el dominio sobre los elementos y los animales. Sus gentes, sencillas y tranquilas, tienen como único objetivo conseguir paz y sabiduría..."
+     ),
+     "Ponyo": MovieTranslation(
+          title = "Ponyo en el acantilado",
+          description = "La historia se centra en Sosuke, un chico de 5 años, y su relación con una princesa pez que ansía convertirse en un ser humano. Libre adaptación de \"La sirenita\" que obtuvo muy buena acogida de la crítica en el Festival de Venecia 2008."
+     ),
+     "The Secret World of Arrietty": MovieTranslation(
+          title = "Arrietty y el mundo de los diminutos",
+          description = "En una casita oculta bajo las tablas del suelo de una mansión campestre, vive una familia de seres diminutos, de apenas 10 cm. de altura, que tienen la norma de no dejarse ver nunca por los seres humanos; sin embargo, su tranquila existencia cambia cuando Arrietty, una audaz adolescente, es vista accidentalmente por un niño que se acaba de establecer en la casa debido a su delicada salud. Al tiempo que entre ambos surge una gran amistad, la existencia de los diminutos seres se verá peligrosamente amenazada... Adaptación del cuento infantil \"The Borrowers\", escrito por Mary Norton y ambientado en el Tokio contemporáneo."
+     ),
+     "From Up on Poppy Hill" : MovieTranslation(
+          title = "La colina de las amapolas",
+          description = "Japón, año 1963. Umi Matsuzaki es una estudiante de instituto que, en ausencia de su madre, cuida a sus dos hermanos y de su abuela a la par que administra un hostal de estilo occidental, el Coquelicot Manor, en lo alto de una colina y cercano al mar. La chica compagina tranquilamente sus responsabilidades con su vida escolar. Un día conoce a Shun Kazama, miembro del club de periodismo, y Shiro Mizunuma, presidente del consejo de estudiantes. Ambos son representantes del Quartier Latin, un edificio antiguo que alberga las diferentes asociaciones de estudiantes y que corre el peligro de ser demolido. Entre Umi y Kazama surgirá una profunda amistad que podría verse complicada con el inesperado descubrimiento de un secreto del pasado. Juntos descubrirán una forma de convivir entre el turbio pasado, el difícil presente y el esperanzador futuro en un momento del tiempo donde el Japón empezaba a levantar cabeza."
+     ),
+     "The Wind rises" : MovieTranslation(
+          title = "El viento se levanta",
+          description = "Jiro Horikoshi, que sueña con volar y diseñar hermosos aviones, se inspira en el famoso diseñador aeronáutico italiano Caproni. Corto de vista desde niño y por ello incapaz de volar, se une a la división aeronáutica de una compañía de ingeniería en 1927. Su genio pronto es reconocido y se convierte en uno de los más prestigiosos diseñadores aeronáuticos. Film biográfico que recrea hechos históricos que marcaron su vida, como el terremoto de Kanto de 1923, la Gran Depresión, la epidemia de tuberculosis y la entrada de Japón en la Segunda Guerra Mundial."
+     ),
+     "The Tale of Princess Kaguya" : MovieTranslation(
+          title = "El cuento de la princesa Kaguya",
+          description = "Basada en un cuento popular japonés anónimo del siglo IX, \"El cortador de bambú\". La historia comienza cuando una pareja de ancianos campesinos encuentra a una niña diminuta dentro de una planta de bambú, y deciden adoptarla como si fuera su hija. Pasan los años, y rápidamente se convierte en una hermosa mujer pretendida por muchos hombres poderosos."
+     ),
+     "When Marnie Was There" : MovieTranslation(
+          title = "El recuerdo de Marnie",
+          description = "Anna es una chica solitaria, sin amigos, que vive con sus padres adoptivos. Un día es enviada con el señor y la señora Pegg. Allí, donde se extienden las dunas de arena, conoce a una chica llamada Marnie, quien pronto se convertirá en su mejor amiga. Habiendo aprendido muchas cosas sobre la amistad, Anna se dará cuenta de que Marnie no es quien parece..."
+     ),
+     "Earwig and the Witch" : MovieTranslation(
+          title = "Earwig y la bruja",
+          description = "No a todos los huérfanos les gusta vivir en el orfanato de St. Morwald's, pero Earwig lo adora. Tiene todo lo que necesita para ser una muchacha feliz desde el primer día que se abrieron para ella las puertas del centro. Sin embargo, todo cambia el día que Bella Yaga y Mandrake visitan St. Morwald disfrazados de padres adoptivos. Earwig se traslada a su domicilio, una enorme casa repleta de habitaciones invisibles, pociones y libros de hechizos, con magia en cada rincón. Aunque muchos niños hubiesen huido de allí sin pensárselo dos veces, Earwig decide investigar. Empleando su propio ingenio (y algo de ayuda de un gato parlanchín), decide demostrar a todo el mundo quién manda en la casa.\n\nAdaptación de la novela homónima de Diana Wynne Jones. Es el primer largometraje de animación por ordenador de Studio Ghibli, dirigido por Goro Miyazaki, escrito y supervisado por Hayao Miyazaki y producido por Toshio Suzuki."
+     ),
+     "The Boy and the Heron" : MovieTranslation(
+          title = "El chico y la garza",
+          description = "Mahito, un joven de 12 años, lucha por asentarse en una nueva ciudad tras la muerte de su madre. Sin embargo, cuando una garza parlante informa a Mahito de que su madre sigue viva, entra en una torre abandonada en su busca, lo que le lleva a otro mundo. El título de la película se basa en la novela de 1937, 'Kimitachi wa Dō Ikiru ka' escrita por Yoshino Genzaburō pero la película presenta una historia original que no guarda relación con la novela."
+     ),
+     "The Red Turtle": MovieTranslation(
+          title = "La tortuga roja",
+          description = "Historia muda sobre un náufrago en una isla tropical desierta, poblada de tortugas, cangrejos y aves. La película cuenta las grandes etapas de la vida de un ser humano. Debut en el largometraje del animador Michael Dudok de Wit (ganador del Oscar por su cortometraje \"Father and Daughter\"). Una coproducción de varias productoras francesas y Studio Ghibli."
+     )
+} 
